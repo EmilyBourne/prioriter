ddcd95e8e8a8210a95a5c18ce33abe0a80aac4d8
EmilyBoune <louise.bourne@gmail.com>
2019-12-12 18:26:28 +0100

    Ensure exit value is set after tests so this file can be used as a git hook

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 curdir=`dirname "$0"`/
-echo $curdir
 
 available_tests=$(ls ${curdir}tests)
 
-if [ -z "available_tests" ]
+if [ -z "$available_tests" ]
 then
-    cd ..;
+    cd $curdir/..;
     make tests;
+    if [ $? -ne 0 ]
+    then
+        echo "Could not compile"
+        exit 1
+    fi
     available_tests=$(ls ${curdir}tests)
 fi
+
+result=0
 
 for test in $available_tests
 do
     echo "Running $test"
     ${curdir}tests/$test
+    if [ $? -ne 0 ]
+    then
+        result=1
+    fi
 done
+
+exit $result
